e07d2e6e1f45f97d6d064d5866ecba8c15fdcd68
Michal Sitko <pierwszy1@gmail.com>
2011-06-26 15:30:55 +0200

    Added stepping mode

--- src/memtrace.cpp
+++ src/memtrace.cpp
@@ -30,8 +30,8 @@ class syscall_observer{
 	thread * observed_thread;
 	public:
 	syscall_observer(thread * observed_thread) : observed_thread(observed_thread){}
-	virtual void handle_enter(user_regs_struct * regs) = 0;
-	virtual void handle_return(user_regs_struct * regs) = 0;
+	virtual bool handle_enter(user_regs_struct * regs) = 0;
+	virtual bool handle_return(user_regs_struct * regs) = 0;
 	virtual ~syscall_observer(){}
 };
 
@@ -39,7 +39,7 @@ class thread{
 	public:
 	thread(pid_t pid);
 	thread(){}
-	void received(user_regs_struct * regs);
+	bool received(user_regs_struct * regs);
 	bool returning;
 	syscall_observer * observer;
 	pid_t id;
@@ -55,7 +55,8 @@ class mmap_pgoff_observer : public syscall_observer{
 	int orig_esi;
 	public:
 	mmap_pgoff_observer(thread * observed_thread) : syscall_observer(observed_thread){}
-	void handle_enter(user_regs_struct * regs){
+
+	bool handle_enter(user_regs_struct * regs){
 		size = regs->ecx;
 		filename = descriptors[regs->edi];
 		string msg;
@@ -67,9 +68,10 @@ class mmap_pgoff_observer : public syscall_observer{
 		msg += (regs->esi & MAP_ANONYMOUS) ? " (anonymous)" : " from the file " + filename;
 		observed_thread->print_notification(msg);
 		orig_esi = regs->esi;
+		return true;
 	}
 
-	void handle_return(user_regs_struct * regs){
+	bool handle_return(user_regs_struct * regs){
 		string msg;
 		if(is_ok(regs->eax)){
 			if(regs->esi & MAP_PRIVATE)
@@ -85,6 +87,7 @@ class mmap_pgoff_observer : public syscall_observer{
 			
 		msg += (orig_esi & MAP_ANONYMOUS) ? " (anonymous)" : " from the file " + filename;
 		observed_thread->print_notification(msg);
+		return true;
 	}
 
 	private:
@@ -94,40 +97,47 @@ class mmap_pgoff_observer : public syscall_observer{
 class brk_observer : public syscall_observer{
 	public:
 	brk_observer(thread * observed_thread) : syscall_observer(observed_thread){}
-	void handle_enter(user_regs_struct * regs){
+	
+	bool handle_enter(user_regs_struct * regs){
 		int diff = regs->ebx - observed_thread->current_break;
 		if(regs->ebx){ //process is trying to change break
 			if(diff>0)
 				observed_thread->print_notification("Process is trying to allocate " + format(diff));
 			else
 				observed_thread->print_notification("Process is trying to deallocate " + format(-diff));
+			return true;
 		}
+		return false;
 	}
 	
-	void handle_return(user_regs_struct * regs){
+	bool handle_return(user_regs_struct * regs){
 		int diff = regs->eax-observed_thread->current_break;
 		if(observed_thread->current_break != -1 && diff!=0){
 			if(diff>0)
 				observed_thread->print_notification("Process has allocated " + format(diff));
 			else
 				observed_thread->print_notification("Process has deallocated " + format(-diff));
+			return true;
 		}
 		observed_thread->current_break = regs->eax;
-
+		return false;
 	}
 };
 
 class open_observer : public syscall_observer{
 	string filename;
 	public:
+
 	open_observer(thread * observed_thread) : syscall_observer(observed_thread){}
-	void handle_enter(user_regs_struct * regs){
+	bool handle_enter(user_regs_struct * regs){
 		filename = load_string(observed_thread->id, regs->ebx);
+		return false;
 	}
 
-	void handle_return(user_regs_struct * regs){
+	bool handle_return(user_regs_struct * regs){
 		if(is_ok(regs->eax))
 			descriptors[regs->eax] = filename;
+		return false;
 	}
 
 };
@@ -135,16 +145,18 @@ class open_observer : public syscall_observer{
 class old_mmap_observer : public syscall_observer{
 	mmap_arg_struct mmap_arg;
 	public:
+
 	old_mmap_observer(thread * observed_thread) : syscall_observer(observed_thread){}
-	void handle_enter(user_regs_struct * regs){
+	bool handle_enter(user_regs_struct * regs){
 		load_struct(&mmap_arg, observed_thread->id, regs->ebx);
 		if(descriptors[mmap_arg.fd].find("/dev/shm/") == 0)
 			observed_thread->print_notification("Process is trying to map " + format(mmap_arg.len) + " from shared memory");
 		else
 			observed_thread->print_notification("Process is trying to map " + format(mmap_arg.len) + " from the file " + descriptors[mmap_arg.fd]);
+		return true;
 	}
 
-	void handle_return(user_regs_struct * regs){
+	bool handle_return(user_regs_struct * regs){
 		if(is_ok(regs->eax)){
 			if(descriptors[mmap_arg.fd].find("/dev/shm/") == 0)
 				observed_thread->print_notification("Process has mapped " + format(mmap_arg.len) + " from shared memory");
@@ -156,19 +168,23 @@ class old_mmap_observer : public syscall_observer{
 			else
 				observed_thread->print_notification("Process has failed " + format(mmap_arg.len) + " from the file " + descriptors[mmap_arg.fd]);
 		}
+		return true;
 	}
 };
 
 class munmap_observer : public syscall_observer{
 	public:
+	
 	munmap_observer(thread * observed_thread) : syscall_observer(observed_thread){}
-	void handle_enter(user_regs_struct * regs){
+	bool handle_enter(user_regs_struct * regs){
 		observed_thread->print_notification("Process is trying to unmap " + format(regs->ecx));
+		return true;
 	}
 
-	void handle_return(user_regs_struct * regs){
+	bool handle_return(user_regs_struct * regs){
 		if(is_ok(regs->eax))
 			observed_thread->print_notification("Process has unmapped " + format(regs->ecx));
+		return true;
 	}
 };
 
@@ -178,7 +194,11 @@ map<int, thread *> threads;
 
 thread::thread(pid_t pid) : returning(false), observer(NULL), id(pid), current_break(-1), active(false) {}
 
-void thread::received(user_regs_struct * regs){
+/**
+ * @return true if any action that user should be notified about was performed
+ */
+bool thread::received(user_regs_struct * regs){
+	bool ret = false;
 	if(!returning){
 		switch(regs->orig_eax){
 			case 5:
@@ -198,16 +218,17 @@ void thread::received(user_regs_struct * regs){
 			break;
 		}
 		if(observer)
-			observer->handle_enter(regs);
+			ret = observer->handle_enter(regs);
 	}else{
 		if(observer){
-			observer->handle_return(regs);
+			ret = observer->handle_return(regs);
 			delete observer;
 			observer = NULL;
 		}
 	}
 
 	returning = !returning;
+	return ret;
 }
 
 void thread::print_notification(const string & notification){
@@ -237,15 +258,19 @@ void exit_with_perror(const char * msg){
 	exit(1);
 }
 
+void view_console_help(){
+	cout << "memtrace console commands:" << endl << " next (abbraviated: n) - step execution" << endl << " system (sys s) - system memory info" << endl <<
+	" process (proc p) - process memory info" << endl << " run (r) - exit stopping mode, continue tracing, but not in stepping mode" << endl <<
+	" detach (d) - stop tracing mode, process which is observed will continue execution" << endl << " quit (q) - exit memtrace and process which is observed" << endl <<
+	" help (h) - display help information" << endl;
+}
+
 void view_help(){
 	cout << "memtrace" << endl << "Copyright Michal Sitko" << endl << endl;
 	cout << "usage: memtrace [-hsfo] command [arg ...]" << endl << "-h display help information" << endl
 	<< "-s stepping mode, memtrace will stop executing and enter memtrace console after every syscall" << endl << "-f follow threads created by process being observed" << endl
 	<< "-o file - send memtrace output to file instead of stderr" << endl << endl;
-	cout << "memtrace console commands:" << endl << " next (abbraviated: n) - step execution" << endl << " system (sys s) - system memory info" << endl <<
-	" process (proc p) - process memory info" << endl << " run (r) - exit stopping mode, continue tracing, but not in stepping mode" << endl <<
-	" detach (d) - stop tracing mode, process which is observed will continue execution" << endl << " quit (q) - exit memtrace and process which is observed" << endl <<
-	" help (h) - display help information" << endl;
+	view_console_help();
 	exit(0);
 }
 
@@ -260,8 +285,6 @@ pid_t handle_child(pid_t parent_id){
 	unsigned long child_id;
 	ptrace(PTRACE_GETEVENTMSG, parent_id, NULL, &child_id);
 	threads[child_id] = new thread(child_id);
-	//if(ptrace(PTRACE_ATTACH, (pid_t) child_id, NULL, NULL) == -1)
-	//	perror("ptrace_attach");
 	threads[parent_id]->print_notification("Started new thread with id: " + str(child_id));
 	return (pid_t) child_id;
 }
@@ -282,12 +305,12 @@ int main(int argc, const char *argv[]){
 	int i = 1;
 	while(i<argc){
 		if(argv[i][0] == '-'){
-			if(i == argc-1)
-				exit_with_msg("!Proper invocation: ./memtrace command [args].\nFor more information: ./memtrace -h");
-
 			string s(argv[i]);
 			if(s.find("h") != string::npos)
 				view_help(); // view_help does not return
+
+			if(i == argc-1)
+				exit_with_msg("!Proper invocation: ./memtrace command [args].\nFor more information: ./memtrace -h");
 			if(s.find("s") != string::npos)
 				stepping_mode = true;
 			if(s.find("f") != string::npos)
@@ -327,15 +350,22 @@ int main(int argc, const char *argv[]){
 		wait(NULL);
 
 		threads[id] = new thread(id);
+		threads[id]->active = true;
 
     		user_regs_struct regs;
-		ptrace(PTRACE_SETOPTIONS, id, NULL, PTRACE_O_TRACECLONE | PTRACE_O_TRACEEXIT);
+
+		if(follow_threads)
+			ptrace(PTRACE_SETOPTIONS, id, NULL, PTRACE_O_TRACECLONE | PTRACE_O_TRACEEXIT);
+		else
+			ptrace(PTRACE_SETOPTIONS, id, NULL, PTRACE_O_TRACEEXIT);
 
  		if(ptrace(PTRACE_SYSCALL, id, NULL, NULL) == -1)
  			perror("ptrace_syscall");
 		
 		int status;
+		bool console = false;
 		pid_t child_id;
+		string in;
 		while(1){ // main loop
 			//cout << "MAIN LOOP" << endl;
 			id = waitpid(-1, &status, __WALL); // you must use the __WALL option inorder to receive notifications from threads created by the child process
@@ -362,20 +392,29 @@ int main(int argc, const char *argv[]){
 			ptrace(PTRACE_GETSIGINFO, id, NULL, &siginfo);
 			child_id = -1;
 			if(siginfo.si_signo == SIGTRAP){ // action caught by ptrace
-				if(status>>16 == PTRACE_EVENT_CLONE)
+				if(follow_threads && status>>16 == PTRACE_EVENT_CLONE)
 					child_id = handle_child(id);
 
-				threads[id]->received(&regs);
+				console = threads[id]->received(&regs);
 			}else{ // real signal was delivered
 				if(siginfo.si_signo == 19 && !threads[id]->active) // new thread is initially stopped with a SIGSTOP, there's no sense in displaying info about this
 					threads[id]->active = true;
 				else
 					threads[id]->print_notification("Received signal number " + str(siginfo.si_signo));
 			}
-			
-			//if(child_id > 0)
-			//	if(ptrace(PTRACE_SYSCALL, child_id, NULL, NULL) == -1)
-			//		perror(string("ptrace_syscall2 " + str(child_id)).c_str());
+
+			while(console){
+				cout << "(memtrace) ";
+				cin >> in;
+				if(in == "help" || in == "h"){
+					view_console_help();
+					continue;
+				}
+				if(in == "next" || in == "n")
+					break;
+				cout << "Undefinded command: \"" << in << "\". Try \"help\"" << endl;
+			}
+				
 			
 			if(ptrace(PTRACE_SYSCALL, id, NULL, NULL) == -1)
 				perror("ptrace_syscall");
